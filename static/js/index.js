bf495a03f108f441da5438cef00f0df3e36b54fd
Kai Wei(联通上海市本部) <sh-weikai@chinaunicom.cn>
2018-07-30 20:35:16 +0800

    commit front

--- static/js/index.js
+++ static/js/index.js
@@ -14,7 +14,7 @@ $(function(){
      */
     function getDataList(jsonData){
         $.ajax({
-            url:"/index/leftfrm",
+            url:"/product_detail",
             type:"post",
             data:jsonData,
             dataType:"json",
@@ -23,22 +23,58 @@ $(function(){
                 if(data==null){
                     $("#m-list-search_results_id").html("<span class='m-list-search__result-message'>对不起，没有检索到相关数据 </span>");
                 }else{
-                    var content ='<a href="/detail" target="_blank">'+
-                                    '<span class="m-list-search__result-category m-list-search__result-category--first">'+
-                                        '松子<i style="font-size:10px;margin-left:15px;">(明 景泰6年(1455))</i>'+
-                                    '</span>'+
-                                '</a>'+
-                                '<a href="#" class="m-list-search__result-item">'+
-                                    '<span class="m-list-search__result-item-icon">'+
-                                        '<i class="flaticon-interface-3 m--font-warning"></i>'+
-                                    '</span>'+
-                                    '<span class="m-list-search__result-item-text">'+
-                                        '树皮無龍鳞而稍光滑枝上結松毬大如茶甌其中含寶有二三百粒者'+
-                                    '</span>'+
-                                '</a>';
+                    // var content ='<div style="margin-bottom:10px;">'+
+                    //                 '<a href="/detail" target="_blank" class="m-list-search__result-item">'+
+                    //                     '<span class="m-list-search__result-item-icon">'+
+                    //                         '<i class="flaticon-paper-plane m--font-info"></i>'+
+                    //                     '</span>'+
+                    //                     '<span class="m-list-search__result-category m-list-search__result-category--first">'+
+                    //                         '松子<i style="font-size:10px;margin-left:15px;">(明 景泰6年(1455))</i>'+
+                    //                     '</span>'+
+                    //                     '<span class="m-list-search__result-item-text">'+
+                    //                         '来源志书1，来源志书2'+
+                    //                     '</span>'+
+                    //                 '</a>'+
+                    //                 '<a href="#" class="m-list-search__result-item">'+
+                                        
+                    //                 '</a></div>';
+                    var content ='<ul class="m-nav m-nav--inline">'+
+                                    '<li class="m-nav__item">'+
+                                        '<a href="/detail" target="_blank" class="m-nav__link">'+
+                                            '<i class="flaticon-paper-plane m--font-info" style="padding-right:10px;"></i>'+
+                                            '<span class="m-nav__link-text" style="font-size:16px;">'+
+                                                '松子'+    
+                                            '</span>'+
+                                        '</a>'+
+                                    '</li>'+
+                                    '<li class="m-nav__item">'+
+                                        '<span class="m-nav__link-text" style="padding-left:10px;">'+
+                                            '来源志书1<i style="margin-left:15px;">(明 景泰6年(1455))</i>'+
+                                        '</span>'+
+                                    '</li>'+
+                                '</ul>';
 
                     $("#m-list-search_results_id").empty();
                     $("#m-list-search_results_id").append(content);
+                    $("#m-list-search_results_id").append(content);
+                    $("#m-list-search_results_id").append(content);
+                    $("#m-list-search_results_id").append(content);
+                    $("#m-list-search_results_id").append(content);
+                    $("#m-list-search_results_id").append(content);
+                    $("#m-list-search_results_id").append(content);
+                    $("#m-list-search_results_id").append(content);
+                    $("#m-list-search_results_id").append(content);
+                    $("#m-list-search_results_id").append(content);
+                    
+                    var btn = '<a style="float:right;" class="btn btn-info m-btn m-btn--custom m-btn--icon m-btn--pill m-btn--air">'+
+                        '<span>'+
+                            '<i class="la la-plus"></i>'+
+                            '<span>'+
+                                '点我查看更多'+
+                            '</span>'+
+                        '</span>'+
+                    '</a>';
+                    $("#m-list-search_results_id").append(btn);
                 }
             },
             error:function(data){
