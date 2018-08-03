1675552a420bdab5681405c261b3090b1bd7e329
Zhenhuan Hua <zhenhuan.hua@autodesk.com>
2018-08-03 17:41:31 +0800

    add cdbd qurey & configure port in config file

---
+++ cbdb.py
@@ -0,0 +1,28 @@
+from flask import request, abort
+from enum import Enum, unique
+from server import app
+from util import *
+import requests
+
+@unique
+class CBDBParam(Enum):
+    name = "name"
+
+class CBDB:
+
+    def __check_cbdb_params(self, args):
+        check_url_params(args, CBDBParam)
+        if CBDBParam.name.value not in args:
+            abort(500, "name must has a value")
+
+    def get_cbdb_info(self):
+        self.__check_cbdb_params(request.args)
+
+        name = request.args.get(CBDBParam.name.value)
+
+        return query_cbdb(name)
+
+@lru_cache()
+@respjson
+def query_cbdb(name):
+    return requests.get(f"https://cbdb.fas.harvard.edu/cbdbapi/person.php?name={name}&o=json")
